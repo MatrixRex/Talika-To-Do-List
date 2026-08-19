bc0ac7bb246ea1923f58fbf842507f57085c6726
MatrixRex <matrixrex05@gmail.com>
2026-08-19 21:05:44 +0600

    feat(dev): add QR code support and enable host listening in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    qrcode()
+  ],
+  server: {
+    host: true,
+  },
   test: {
     environment: 'jsdom',
   },
 })
+
